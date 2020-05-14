6c25fb3ab1ddd1eca3ca7ee7971b84f82f055e6f
Harivignesh-A <65211532+Harivignesh-A@users.noreply.github.com>
2020-05-14 19:14:30 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -5,9 +5,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+    console.log("Good Bye "+names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+    console.log("Hello "+names[i]);
   }
 }
 
